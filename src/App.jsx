a1a9fe850aaec227ab1514788bf95a60d6bb120d
Harshit <shanumor32@gmail.com>
2023-09-18 14:50:33 +0530

    created logic of game and show winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import Board from './Components/Board';
+import { useState } from 'react';
 import './Components/style.scss';
+import { calculateWinner } from './winner';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const [isNext, setIsNext] = useState(false);
+  const winner = calculateWinner(squares);
+
+  const nextPlayer = isNext ? 'X' : 'O';
+  const statusMessage = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${nextPlayer}`;
+
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winner) {
+      return;
+    }
+
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+
+    setIsNext(currentIsXNext => !currentIsXNext);
+  };
+
   return (
     <div className="app">
-      <Board />
+      <h2>{statusMessage}</h2>
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
